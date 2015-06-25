9e1e93477c54d412a540dc3b9518c6110a2a7f55
huzongyao <hzy3774@foxmail.com>
2015-06-25 14:46:03 +0800

    add some lint

--- Nesoid/src/com/androidemu/nes/wrapper/Wrapper5.java
+++ Nesoid/src/com/androidemu/nes/wrapper/Wrapper5.java
@@ -20,44 +20,37 @@ class Wrapper5 {
 		BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
 		if (adapter == null)
 			return false;
-		return (adapter.getScanMode() ==
-				BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE);
+		return (adapter.getScanMode() == BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE);
 	}
 
 	public static final boolean supportsMultitouch(Context context) {
 		if (Wrapper.SDK_INT < 7)
 			return true;
 
-		return context.getPackageManager().hasSystemFeature(
-				PackageManager.FEATURE_TOUCHSCREEN_MULTITOUCH);
+		return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_TOUCHSCREEN_MULTITOUCH);
 	}
 
 	public static final int MotionEvent_getPointerCount(MotionEvent event) {
 		return event.getPointerCount();
 	}
 
-	public static final int MotionEvent_getPointerId(MotionEvent event,
-			int pointerIndex) {
+	public static final int MotionEvent_getPointerId(MotionEvent event, int pointerIndex) {
 		return event.getPointerId(pointerIndex);
 	}
 
-	public static final int MotionEvent_findPointerIndex(MotionEvent event,
-			int pointerId) {
+	public static final int MotionEvent_findPointerIndex(MotionEvent event, int pointerId) {
 		return event.findPointerIndex(pointerId);
 	}
 
-	public static final float MotionEvent_getX(MotionEvent event,
-			int pointerIndex) {
+	public static final float MotionEvent_getX(MotionEvent event, int pointerIndex) {
 		return event.getX(pointerIndex);
 	}
 
-	public static final float MotionEvent_getY(MotionEvent event,
-			int pointerIndex) {
+	public static final float MotionEvent_getY(MotionEvent event, int pointerIndex) {
 		return event.getY(pointerIndex);
 	}
 
-	public static final float MotionEvent_getSize(MotionEvent event,
-			int pointerIndex) {
+	public static final float MotionEvent_getSize(MotionEvent event, int pointerIndex) {
 		return event.getSize(pointerIndex);
 	}
 }
